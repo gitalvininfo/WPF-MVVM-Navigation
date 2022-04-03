a198f019a418acd68ac9293a9f3ec59832c2c2b2
Alvin Yanson <yansonalvin29@gmail.com>
2022-04-03 12:58:59 +0800

    Add project files.

---
+++ SwitchingMVVMNavigator/ViewModels/AccountViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SwitchingMVVMNavigator.Commands;
+using SwitchingMVVMNavigator.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwitchingMVVMNavigator.ViewModels
+{
+    public class AccountViewModel : ViewModelBase
+    {
+        public string WelcomeMessage => "Welcome to my account.";
+        public ICommand NavigateHomeCommand { get; }
+
+        public AccountViewModel(NavigationStore navigationStore)
+        {
+            NavigateHomeCommand = new NavigateCommand<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore));
+        }
+
+    }
+}
---
+++ SwitchingMVVMNavigator/ViewModels/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SwitchingMVVMNavigator.Commands;
+using SwitchingMVVMNavigator.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwitchingMVVMNavigator.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public string WelcomeMessage => "Welcome to my application.";
+
+
+        public ICommand NavigateAccountCommand { get; }
+
+        public HomeViewModel(NavigationStore navigationStore)
+        {
+            NavigateAccountCommand = new NavigateCommand<AccountViewModel>(navigationStore, () => new AccountViewModel(navigationStore));
+        }
+    }
+}
